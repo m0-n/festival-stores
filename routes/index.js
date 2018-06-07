c7a0db183976ef10f086a4f1d6fb96d7c9ff8ef3
stefanspirkl <spirkl@logos-gmbh.com>
2018-06-07 23:21:45 +0200

    adw

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+
+
 // Do work here
 router.get('/', (req, res) => {
   res.send('Hey! It works!');
